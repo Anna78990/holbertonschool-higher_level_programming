ba6361f560b24f31bb99bed5b4f1f9d8dd74f8d0
Anna78990 <takasuanna1990@gmail.com>
2021-12-21 22:17:53 +0000

    the times to use print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,8 +4,7 @@ for i in range(0, 10):
         if i < n:
             if i >= 0 and n > 1:
                 print(', ', end="")
-            print('{:d}'.format(i), end="")
-            print('{:d}'.format(n), end="")
+            print('{:d}{:d}'.format(i, n), end="")
         else:
             continue
 print('')
