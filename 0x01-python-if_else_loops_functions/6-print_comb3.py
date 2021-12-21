b66881c2c1fbc7b3e6e002da795415ef0e309715
Anna78990 <takasuanna1990@gmail.com>
2021-12-21 22:14:58 +0000

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for n in range(0, 10):
+        if i < n:
+            if i >= 0 and n > 1:
+                print(', ', end="")
+            print('{:d}'.format(i), end="")
+            print('{:d}'.format(n), end="")
+        else:
+           continue
+print('')
