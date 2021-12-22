7cac910fda6efe2d200be7972ac09b8e5c6bda1a
Anna78990 <takasuanna1990@gmail.com>
2021-12-22 14:55:57 +0000

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b > 0:
+        ap = a
+        for i in range(1, b):
+            if ap < 0:
+                ap = abs(ap)
+            ap *= a
+        return ap
+    else:
+        an = a
+        bn = abs(b)
+        for i in range(1, bn):
+            an *= a
+        return 1 / an
