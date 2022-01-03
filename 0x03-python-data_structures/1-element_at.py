107f36753b8e156076ec02e830a9649aca424972
Anna78990 <takasuanna1990@gmail.com>
2022-01-03 14:44:05 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     le = len(my_list)
-    if le < idx or idx < 0:
+    if le <= idx or idx < 0:
         return None
     else:
         return my_list[idx]
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,9 @@
 def no_c(my_string):
     rp = list(my_string)
     le = len(rp) - 1
-    for i in range(0, le):
-        if rp[i] == 'c' or 'C':
+    for i in rp:
+        if i == 'c' or 'C':
             del rp[i]
+            del i
     Rp = "".join(rp)
     return Rp
