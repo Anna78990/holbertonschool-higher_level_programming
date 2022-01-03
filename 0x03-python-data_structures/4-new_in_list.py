c3e2be04d203584b91fc3535d2f2dbb923462207
Anna78990 <takasuanna1990@gmail.com>
2022-01-03 16:54:20 +0000

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    le = len(my_list)
+    if my_list:
+        le = len(my_list)
+    else:
+        return None
     new_list = list(range(le))
-    l = le - 1
     for i in range(0, le):
         new_list[i] = my_list[i]
     if le < idx or idx < 0:
