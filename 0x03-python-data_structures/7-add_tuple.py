2e172c15d93a1621bfb8664d1b2aee6ca2904488
Anna78990 <takasuanna1990@gmail.com>
2022-01-04 16:30:44 +0000

    9-max_integer.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    columns = len(matrix)
-    row = len(matrix[0])
-    for i in range(columns):
-        for j in range(row):
-            if j < row and j > 0:
-                print(" ", end="")
-            print("{:d}".format(matrix[i][j]), end="")
-        print('')
+    result = []
+    Ta = []
+    Tb = []
+    La = list(tuple_a)
+    Lb = list(tuple_b)
+    
+    for i in range(0, 2):
+        if La[i]:
+            Ta.append(La[i])
+        else:
+            Ta.append(0)
+    for j in range(0, 2):
+        if Lb[j]:
+            Tb.append(Lb[j])
+        else:
+            Tb.append(0)
+    f = Ta[0] + Tb[0]
+    s = Ta[1] + Tb[1]
+    result.append(f)
+    result.append(s)
+    res = tuple(result)
+    return res
