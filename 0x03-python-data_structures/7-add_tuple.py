ce351179e93f9ae56c006c11873df04c98f3dfe9
Anna78990 <takasuanna1990@gmail.com>
2022-01-04 18:08:44 +0000

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,18 +3,25 @@ def add_tuple(tuple_a=(), tuple_b=()):
     result = []
     Ta = []
     Tb = []
-    La = list(tuple_a)
-    Lb = list(tuple_b)
-    
-    for i in range(0, 2):
-        if La[i]:
-            Ta.append(La[i])
-        else:
+    if tuple_a:
+        if len(tuple_a) < 2:
+            Ta.append(tuple_a[0])
             Ta.append(0)
-    for j in range(0, 2):
-        if Lb[j]:
-            Tb.append(Lb[j])
         else:
+            for i in range(0, 2):
+                Ta.append(tuple_a[i])
+    else:
+        for i in range(0, 2):
+            Ta.append(0)
+    if tuple_b:
+        if len(tuple_b) < 2:
+            Tb.append(tuple_b[0])
+            Tb.append(0)
+        else:
+            for j in range(0, 2):
+                Tb.append(tuple_b[j])
+    else:
+        for j in range(0, 2):
             Tb.append(0)
     f = Ta[0] + Tb[0]
     s = Ta[1] + Tb[1]
