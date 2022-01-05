ad7ccaad64625f6e103eb4693cc5392e1ad1c69b
Anna78990 <takasuanna1990@gmail.com>
2022-01-05 22:38:55 +0000

    4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    s1 = list(set_1)
+    s2 = list(set_2)
+    s3 = []
+    for i in range(0, len(s1)):
+        s3.append(s1[i])
+    for j in range(0, len(s2)):
+        for l in range(0, len(s3)):
+            if s2[j] == s3[l]:
+                break
+            elif l < (len(s3) - 1):
+                continue
+            else:
+                s3.append(s2[j])
+    return s3
