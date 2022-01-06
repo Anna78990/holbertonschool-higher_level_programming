c3b7b978892a5f3ac760d438ec97653aac2fb8e5
Anna78990 <takasuanna1990@gmail.com>
2022-01-06 15:33:04 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,7 +5,7 @@ def square_matrix_simple(matrix=[]):
         if len(matrix) > 0:
             for i in range(0, len(matrix)):
                 new.append([])
-                for j in range(0, len(matrix)):
+                for j in range(0, len(matrix[i])):
                     a = matrix[i][j]
                     new[i].append(a * a)
             return new
