991532b48b05ab2d5360e53880af8f44e240dfe9
Anna78990 <takasuanna1990@gmail.com>
2022-01-06 12:12:55 +0000

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        removed_value = a_dictionary.pop(key)
-        return removed_value
-    else:
-        return a_dictionary
+    boo = key in a_dictionary
+    if boo == True:
+        a_dictionary.pop(key)
+    return a_dictionary
