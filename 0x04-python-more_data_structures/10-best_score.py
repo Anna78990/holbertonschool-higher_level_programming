e9576e1785d6c5698e09862527e0250d6b7f774f
Anna78990 <takasuanna1990@gmail.com>
2022-01-06 12:41:07 +0000

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        listOfValues = a_dictionary.values()
+        listOfValues = list(listOfValues)
+        mx = max(listOfValues)
+        for k, v in a_dictionary.items():
+            if v == mx:
+                cle = k
+    return cle
