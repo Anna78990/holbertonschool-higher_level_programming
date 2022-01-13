f0c5df8812ba09e64641811aa75298665b7aaf4d
Anna78990 <takasuanna1990@gmail.com>
2022-01-13 00:23:13 +0000

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,19 +1,25 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if the node has cycle or not
+ * @list: pointer to head of list
+ * Return: if it has a cycle return 1, otherwise 0.
+ */
+
 int check_cycle(listint_t *list)
 {
-	int i = 0;
-while (list != NULL)
+listint_t *comp;
+
+while (list)
 {
+	comp = list;
+	while (comp->next)
+	{
+		comp = comp->next;
+		if (list->next == comp->next)
+			return (1);
+	}
 	list = list->next;
-	i++;
 }
-if (i == 0)
-{
-	return (0);
-}
-else
-{
-	return (1);
-}
+return (0);
 }
