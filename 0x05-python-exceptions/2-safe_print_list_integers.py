f1b7e90737559ee72c242e5eeb5031e364c875ea
Anna78990 <takasuanna1990@gmail.com>
2022-01-17 17:07:26 +0000

    for format{:d}

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             s += my_list[i]
             counter += 1
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
         except TypeError:
             i += 1
     print()
