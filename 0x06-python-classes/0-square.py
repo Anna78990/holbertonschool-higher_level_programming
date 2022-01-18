75795293dbbdbf1663ac286003a4dfbd5917f6ea
Anna78990 <takasuanna1990@gmail.com>
2022-01-18 17:16:47 +0000

    1-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """The summary line for a class docstring should fit on one line.
-    This class is the class which do nothing.
-
-    Note:Nothing
-
-    Args:Nothing
-
-    Attributes:Nothing
-
-    """
+    """square"""
     pass
