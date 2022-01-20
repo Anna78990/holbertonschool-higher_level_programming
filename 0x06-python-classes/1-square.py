e0762f5b8d3b8b0e8417ee7dfc1ff03b06fe840a
Anna78990 <takasuanna1990@gmail.com>
2022-01-20 00:26:10 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""This module contains a class representing a Square"""
+
+
 class Square:
+
+    """A class used to represent a Square with the attribute size"""
+
     _size = 0
     def __init__(self, size):
+
+        """
+        initialize square attributes
+        """
+
         self.__size = size
