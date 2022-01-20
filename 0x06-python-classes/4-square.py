e4823e77ddff698507f2e1f3392e73b70d4b8485
Anna78990 <takasuanna1990@gmail.com>
2022-01-20 22:50:40 +0000

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,12 +11,13 @@ class Square:
     """
 
     def __init__(self, size=0):
-        if type(size) is not int:
+        ss = size
+        if type(size) is not int and ss.isdigit is False:
             raise TypeError('size must be an integer')
-        elif size < 0:
+        elif int(size) < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = int(size)
 
     @property
     def size(self):
