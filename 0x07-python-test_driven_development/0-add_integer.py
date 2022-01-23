77fd5776e203411583841264a0be6b46d2b3c0ea
Anna78990 <takasuanna1990@gmail.com>
2022-01-23 22:26:41 +0000

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+"""
+
+This module contains the function which can add the numbers
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    addition
+    """
+    if type(a) != float and type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != float and type(b) != int:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
