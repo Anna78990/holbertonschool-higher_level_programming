c2ecf187cb437aba106ffb6f17497124a9e84ccc
Anna78990 <takasuanna1990@gmail.com>
2022-01-24 14:34:43 +0000

    1-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a class representing a Rectangle"""
+
+
+class Rectangle:
+    """A class used to represent a Rectangle"""
+
+    def __init__(self):
+        """
+        initialize rectangle attributes
+        """
+        pass
+
