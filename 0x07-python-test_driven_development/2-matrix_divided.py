29f82be0db03df9b288ddd2ea40dc562f63bd1ab
Anna78990 <takasuanna1990@gmail.com>
2022-01-26 18:33:28 +0000

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,7 +11,13 @@ def matrix_divided(matrix, div):
     division
     """
     lis = []
+    if type(div) != float and type(div) != int:
+        raise TypeError("div must be a number")
     if len(matrix) == 1:
+        for i in range(0, len(matrix[0])):
+            if type(i) != float and type(i) != int:
+                raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
         lis.append(list(map(lambda x: round(x / div, 2), matrix[0])))
         return lis
     for i in range(0, 2):
@@ -21,8 +27,6 @@ def matrix_divided(matrix, div):
  of integers/floats")
     if len(matrix[0]) != len(matrix[1]):
         raise TypeError("Each row of the matrix must have the same size")
-    if type(div) != float and type(div) != int:
-        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     for n in range(0, 2):
