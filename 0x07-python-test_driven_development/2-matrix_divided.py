6f36030db153fe9eb784e169015598f57891a835
Anna78990 <takasuanna1990@gmail.com>
2022-01-26 19:08:56 +0000

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,32 +6,31 @@ This module contains the function which can devide elements
 """
 
 
-def matrix_divided(matrix, div):
+def matrix_divided(matrix, div):	
     """
     division
     """
     lis = []
-    if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of\
- integers/floats")
-    if type(div) != float and type(div) != int:
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if len(matrix) == 1:
-        for i in matrix[0]:
-            if type(i) != float and type(i) != int:
-                raise TypeError("matrix must be a matrix (list of lists)\
+
+    if type(matrix) is list: 
+        if type(div) != float and type(div) != int:
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+        if len(matrix) == 1:
+            for i in matrix[0]:
+                if type(i) != float and type(i) != int:
+                    raise TypeError("matrix must be a matrix (list of lists)\
  of integers/floats")
-        lis.append(list(map(lambda x: round(x / div, 2), matrix[0])))
+            lis.append(list(map(lambda x: round(x / div, 2), matrix[0])))
+            return lis
+        for i in range(0, 2):
+            for j in matrix[i]:
+                if type(j) != float and type(j) != int:
+                    raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+        if len(matrix[0]) != len(matrix[1]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for n in range(0, 2):
+            lis.append(list(map(lambda x: round(x / div, 2), matrix[n])))
         return lis
-    for i in range(0, 2):
-        for j in matrix[i]:
-            if type(j) != float and type(j) != int:
-                raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
-    if len(matrix[0]) != len(matrix[1]):
-        raise TypeError("Each row of the matrix must have the same size")
-    for n in range(0, 2):
-        lis.append(list(map(lambda x: round(x / div, 2), matrix[n])))
-    return lis
