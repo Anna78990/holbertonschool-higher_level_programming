4f3ad3d648688fa98a9546abf1e0a04540a0e468
Anna78990 <takasuanna1990@gmail.com>
2022-01-27 13:49:33 +0000

    6-max_integer.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,3 +14,15 @@ class TestMaxInteger(unittest.TestCase):
         li2 = [1, 2, 3, 4]
         actual2 = max_integer(li2)
         self.assertEqual(4, actual2)
+        li3 = [4, 1, 2, 3]
+        actual3 = max_integer(li3)
+        self.assertEqual(4, actual3)
+        li4 = [1, 2, 5, 3, 4]
+        actual4 = max_integer(li4)
+        self.assertEqual(5, actual4)
+        li5 = [1, 2, -3, 4]
+        actual5 = max_integer(li5)
+        self.assertEqual(4, actual5)
+        li6 = [-1, -2, -3, -4]
+        actual6 = max_integer(li6)
+        self.assertEqual(-1, actual6)
