7e82addd57f430bef4247913475ec37d7573e73e
Anna78990 <takasuanna1990@gmail.com>
2022-01-27 12:52:37 +0000

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,7 +16,7 @@ def text_indentation(text):
     for i in range(0, len(text)):
         a = i - 1
         if text[i] == " " and \
-           (text[a] == "." or text[a] == "?" or text[a] == ":"):
+           (text[a] == "." or text[a] == "?" or text[a] == ":" or text[a] == " "):
             continue
         print("{}".format(text[i]), end="")
         if text[i] == "." or text[i] == "?" or text[i] == ":":
