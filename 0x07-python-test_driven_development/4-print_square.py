c0cc209807a2a28d41cb21e95864c2ac01a794b5
Anna78990 <takasuanna1990@gmail.com>
2022-01-27 11:07:38 +0000

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,9 +15,8 @@ def print_square(size):
     elif size < 0:
         raise ValueError('size must be >= 0')
     else:
-        if size == 0:
-            print("")
-        for i in range(0, size):
-            for z in range(0, size):
-                print("#", end="")
-            print("")
+        if size != 0:
+            for i in range(0, size):
+                for z in range(0, size):
+                    print("#", end="")
+                print("")
