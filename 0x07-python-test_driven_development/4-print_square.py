bdeacf7f6c9a63dd41e3177c0961c2be7944c47a
Anna78990 <takasuanna1990@gmail.com>
2022-01-27 11:01:17 +0000

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,6 +7,9 @@ This module prints square
 
 
 def print_square(size):
+    """
+    print square
+    """
     if type(size) is not int:
         raise TypeError('size must be an integer')
     elif size < 0:
