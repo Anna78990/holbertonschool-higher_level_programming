0500fcde9ba301985f6d51a3e4d80a8a8f62d1f9
Anna78990 <takasuanna1990@gmail.com>
2022-01-27 13:45:30 +0000

    added testq

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,6 +8,9 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
         li = []
-        expected = None
         actual = max_integer(li)
+        expected = None
         self.assertEqual(expected, actual)
+        li2 = [1, 2, 3, 4]
+        actual2 = max_integer(li2)
+        self.assertEqual(4, actual2)
