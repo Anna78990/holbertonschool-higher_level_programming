b084fe94ec2b4725c92bd0099a0dcb88540ae254
Anna78990 <takasuanna1990@gmail.com>
2022-01-27 11:19:48 +0000

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""
+
+this module prints a text with 2 new lines after each of these
+characters: ., ? and :
+
+"""
+
 
 def text_indentation(text):
+    """
+    prints 2 new lines after each of characters: ".", "?", ":"
+    """
     if type(text) != str:
         TypeError("text must be a string")
     for i in range(0, len(text)):
         a = i - 1
-        if text[i] == " " and (text[a] == "." or text[a] == "?" or\
-            text[a] == ":"):
+        if text[i] == " " and \
+           (text[a] == "." or text[a] == "?" or text[a] == ":"):
             continue
         print("{}".format(text[i]), end="")
         if text[i] == "." or text[i] == "?" or text[i] == ":":
