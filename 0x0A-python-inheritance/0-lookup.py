32bd86aa1dc0f71e670a43657f94df0204aea6b4
Anna78990 <takasuanna1990@gmail.com>
2022-01-31 10:00:13 +0000

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module show the list of attributes"""
+
+
+def lookup(obj):
+
+    """
+    this function show the list of attributes
+    obj: object to list
+    return: list of attributes
+    """
+
+    return dir(obj)
