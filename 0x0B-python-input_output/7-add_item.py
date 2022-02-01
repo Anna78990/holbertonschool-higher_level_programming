1f19a4f5e370c4b19ff3bfc7fe952b0e2409b8c6
Anna78990 <takasuanna1990@gmail.com>
2022-02-01 21:45:12 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import os
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-import os, sys
 
 filename = "add_item.json"
 le = len(sys.argv)
