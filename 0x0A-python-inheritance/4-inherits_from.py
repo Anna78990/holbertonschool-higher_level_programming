3f3739783fe91f5cc7fd15830d63e9135d6f26d2
Anna78990 <takasuanna1990@gmail.com>
2022-02-01 09:48:52 +0000

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""This module check if the object is an instance of a class that inherited"""
+
 
 def inherits_from(obj, a_class):
+    """
+    check if the object is an instance of a class that inherited
+    """
     ac = type(obj)
     if a_class in ac.mro():
         if ac != a_class:
