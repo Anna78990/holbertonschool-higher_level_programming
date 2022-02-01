999c8714902efb5513939e1a4db677ebb7e009b6
Anna78990 <takasuanna1990@gmail.com>
2022-02-01 09:51:03 +0000

    pep8

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""This module contains a class representing a BaseGeometry"""
+
 
 class BaseGeometry:
+    """
+    This module contains a class representing a BaseGeometry
+    """
     pass
