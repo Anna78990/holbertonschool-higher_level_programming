ab9c6e28332f29d1244058bb48f09dbb636269e2
Anna78990 <takasuanna1990@gmail.com>
2022-02-01 09:33:55 +0000

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" check if they are the same class"""
+
 
 def is_same_class(obj, a_class):
+    """
+    check if they are the same class
+    """
     if type(obj) is a_class:
         return True
     else:
