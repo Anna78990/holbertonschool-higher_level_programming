f980ce7320dd61343d4d509dd643ae1fa97af5e4
Anna78990 <takasuanna1990@gmail.com>
2022-02-02 13:07:35 +0000

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,13 +4,10 @@ this module returns the dictionary description with simple data structure
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """
     this returns the dictionary description with simple data structure
     obj(class): class to read
     return: dictionary description
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
