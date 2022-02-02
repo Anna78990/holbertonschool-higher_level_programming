9c790bd1b07617bf6c33b691f9f57a01eb26a78f
Anna78990 <takasuanna1990@gmail.com>
2022-02-02 14:04:54 +0000

    12-main.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this module contains function pascal_triangle
+"""
+
+def pascal_triangle(n):
+    """
+    function to make pascal_triangle
+    """
+    pl = []
+    for i in range(0, n):
+        if i == 0:
+            pl.append([1])
+        elif i == 1:
+            pl.append([1,1])
+        else:
+            m = []
+            for num in range(0, i + 1):
+                if num == 0:
+                    m.append(1)
+                elif num == i:
+                    m.append(1)
+                else:
+                    a = i - 1
+                    b = num - 1
+                    s = pl[a][b] + pl[a][num]
+                    m.append(s)
+            pl.append(m)
+    return pl
