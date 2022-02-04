a7917bfc1029ef1496dd3e617ef09c55d141fa80
Anna78990 <takasuanna1990@gmail.com>
2022-02-04 16:00:18 +0000

    push in bulk

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module contains a class representing Base"""
 
-
+import json
 class Base:
     """A class used to represent a Base"""
     __nb_objects = 0
@@ -12,3 +12,17 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if len(list_dictionaries) != 0:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        filename = "{}.json".format(cls)
+        my_obj = self.to_json_string(list_objs)
+        with open(filename, 'w+') as f:
+            json.dump(my_obj, f)
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 """This module contains a class representing Base"""
 
-
-class Base:
-    """A class used to represent a Base"""
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is None:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-        else:
-            self.id = id
+from models.base import Base
 
 class Rectangle(Base):
 
