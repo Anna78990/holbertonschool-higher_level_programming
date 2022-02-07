c12e769428c6140b0092fb77fcb3f3d35498643b
Anna78990 <takasuanna1990@gmail.com>
2022-02-07 20:49:41 +0000

    doccumentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""This module contains a class representing Base"""
+"""This module contains a class representing Rectangle"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
     """A class used to represent a Rectangle"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.int_valid(width=width, height=height, x=x, y=y)
