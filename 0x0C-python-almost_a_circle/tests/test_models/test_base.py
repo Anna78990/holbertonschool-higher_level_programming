1d7698018dc44f0fced81e2af978a6cda7b36cb0
Anna78990 <takasuanna1990@gmail.com>
2022-02-08 12:03:50 +0000

    unittest

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -21,6 +21,27 @@ class TestBase(unittest.TestCase):
         b = Base(2)
         self.assertEqual(b.id, 2)
 
+    def test_to_string(self):
+        """test method for to_string of base
+        """
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, [])
+        json_dictionary = Base.to_json_string([ { 'id': 12 }])
+        self.assertEqual(json_dictionary, [])
+        json_dictionary = Base.to_json_string([ { 'id': 12 }])        
+        self.assertEqual(json_dictionary, [{"id": 12}])
+
+    def test_from_jason_string(self):
+        """test method from_json_string of base
+        """
+        list_output = Base.from_json_string(None)
+        self.assertEqual(list_output, [])
+        list_output = Base.from_json_string("[]")
+        self.assertEqual(list_output, [])
+        list_output = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(list_output, [{'id': 89}])
+        list_output = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(list_output, [{'id': 89}])
 
 if __name__ == "__main__":
     unittest.main()
