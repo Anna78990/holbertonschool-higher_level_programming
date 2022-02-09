1c210ad1d28c07c074f72a6f870c184a7f5cee6c
Anna78990 <takasuanna1990@gmail.com>
2022-02-09 21:14:29 +0000

    tests sans failures

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,6 +17,35 @@ class TestBase(unittest.TestCase):
         b = Base()
         self.assertEqual(b.id, 1)
 
+    def test_id2(self):
+        b = Base()
+        self.assertEqual(b.id, 2)
+
+    def test_id3(self):
+        b = Base(98)
+        self.assertEqual(b.id, 98)
+
+    def test_to_string(self):
+        """test method for to_string of base
+        """
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+        json_dictionary = Base.to_json_string([ { 'id': 12 }])
+        self.assertEqual(json_dictionary, "[{\"id\": 12}]")
+        json_dictionary = Base.to_json_string([ { 'id': 12 }])        
+        self.assertEqual(json_dictionary, "[{\"id\": 12}]")
+
+    def test_from_jason_string(self):
+        """test method from_json_string of base
+        """
+        list_output = Base.from_json_string(None)
+        self.assertEqual(list_output, [])
+        list_output = Base.from_json_string("[]")
+        self.assertEqual(list_output, [])
+        list_output = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(list_output, [{'id': 89}])
+        list_output = Base.from_json_string('[{ "id": 89 }]')
+        self.assertEqual(list_output, [{'id': 89}])
 
 if __name__ == "__main__":
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -16,7 +16,142 @@ class TestRectangle(unittest.TestCase):
         """
         r = Rectangle(1, 2)
         r2 = Rectangle(1, 2, 0, 0, 1)
+        self.assertEqual(r.height, r2.height)
 
+    def test_init_rectangle2(self):
+        """test method for initialisation of rectangle
+        """
+        r = Rectangle(1, 2, 3)
+        r2 = Rectangle(1, 2, 3, 0)
+        self.assertEqual(r.width, r2.width)
+
+    def test_init_rectangle3(self):
+        """test method for initialisation of rectangle
+        """
+        r = Rectangle(1, 2, 3, 4)
+        r2 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r.x, r2.x)
+
+    def test_init_rectangle4(self):
+        """test method for initialisation of rectangle
+        """
+        r = Rectangle(1, 2, 3, 4, 5)
+        r2 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r.id, r2.id)
+    
+    def test_init_rectangle_type_error(self):
+        """test method for check type error case of rectangle
+        """
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
+    def test_init_rectangle_value_error(self):
+        """test method for check value error case of rectangle
+        """
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
+
+    def area(self):
+        """test method for the calculation of area
+        """
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2, 20)
+  
+    def test_update_by_args(self):
+        """test method update the instance by args
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update()
+        r2 = Rectangle(10, 10, 10, 10, 1)
+        self.assertEqual(r1.width, r2.width)
+
+    def test_update_by_args2(self):
+        """test method update the instance by args
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 1)
+        r2 = Rectangle(1, 10, 10, 10, 89)
+        self.assertEqual(r1.id, r2.id)
+
+    def test_update_by_args3(self):
+        """test method update the instance by args
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 1, 2)
+        r2 = Rectangle(1, 2, 10, 10, 89)
+        self.assertEqual(r1.width, r2.width)
+
+    def test_update_by_args4(self):
+        """test method update the instance by args
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 1, 2, 3)
+        r2 = Rectangle(1, 2, 3, 10, 89)
+        self.assertEqual(r1.height, r2.height)
+
+    def test_update_by_args5(self):
+        """test method update the instance by args
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 1, 2, 3, 4)
+        r2 = Rectangle(1, 2, 3, 4, 89)
+        self.assertEqual(r1.x, r2.x)
+
+    def test_update_by_kwargs(self):
+        """test method update the instance by **kwargs
+       	"""
+        r1 = Rectangle(10, 10, 10, 10, 1)
+        r1.update(**{ 'id': 89 })
+        r2 = Rectangle(10, 10, 10, 10, 89)
+        self.assertEqual(r1.id, r2.id)
+
+    def test_update_by_kwargs2(self):
+        """test method update the instance by **kwargs
+       	"""
+        r1 = Rectangle(10, 10, 10, 10, 1)
+        r1.update(**{ 'id': 89, 'width': 1 })
+        r2 = Rectangle(1, 10, 10, 10, 89)
+        self.assertEqual(r1.width, r2.width)
+
+    def test_update_by_kwargs3(self):
+        """test method update the instance by **kwargs
+       	"""
+        r1 = Rectangle(10, 10, 10, 10, 1)
+        r1.update(**{ 'id': 89, 'width': 1, 'height': 2 })
+        r2 = Rectangle(1, 2, 10, 10, 89)
+        self.assertEqual(r1.height, r2.height)
+
+    def test_update_by_kwargs4(self):
+        """test method update the instance by **kwargs
+       	"""
+        r1 = Rectangle(10, 10, 10, 10, 1)
+        r1.update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 })
+        r2 = Rectangle(1, 2, 3, 10, 89)
+        self.assertEqual(r1.x, r2.x)
+
+    def test_update_by_kwargs5(self):
+        """test method update the instance by **kwargs
+       	"""
+        r1 = Rectangle(10, 10, 10, 10, 1)
+        r1.update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 })
+        r2 = Rectangle(1, 2, 3, 4, 89)
+        self.assertEqual(r1.y, r2.y)
 
 if __name__ == "__main__":
     unittest.main()
