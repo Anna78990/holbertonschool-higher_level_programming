bf1d5d19a2c48b55d5f0f4fb5499650b52fd99b9
Anna78990 <takasuanna1990@gmail.com>
2022-03-08 17:11:26 +0000

    sans =

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,6 @@
 -- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_temp, RANK() OVER(ORDER BY avg_temp DESC) AS r
-FROM temperatures GROUP BY city
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures AS t1
+GROUP BY city
+INNER JOIN (SELECT t1.city AS city, RANK() OVER(ORDER BY t1.avg_temp DESC) AS r FROM t1) AS B
+ON t1.city = B.city
