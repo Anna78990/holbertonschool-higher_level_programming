f892e7dd044f8a9afb25e06780c7366c04742f90
Anna78990 <takasuanna1990@gmail.com>
2022-03-09 13:21:36 +0000

    intersect

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT name FROM states UNION EXCEPT SELECT name FROM cities ORDER BY cities.id
+SELECT name FROM states INTERSECT SELECT name FROM cities ORDER BY cities.id
