f846c2408126ecebc8b2984e8d30250f77854142
Anna78990 <takasuanna1990@gmail.com>
2022-03-09 20:28:03 +0000

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+--  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT title, name
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tsg
+ON tsg.show_id = ts.id
+LEFT JOIN tv_genres AS tg
+ON tg.id = tsg.genre_id
+ORDER BY title ASC
