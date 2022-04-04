1bce95d9aff099e06a9300feab636e591441c95c
Anna78990 <takasuanna1990@gmail.com>
2022-04-04 13:18:25 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 const argv = process.argv;
-if (argv[2]) {
-  if (argv[3]) {
-    console.log(comp(argv));
-  } else if (parseInt(argv[2]) === 1) {
-    console.log('0');
-  } else {
-    console.log(parseInt(argv[2]));
-  }
+if (argv[2] && argv[3]) {
+  console.log(comp(argv));
 } else {
   console.log(0);
 }
 
 function comp (args) {
   let n = parseInt(args[2]);
+  let pi;
   for (let i = 3; i < args.length; i++) {
-    if (n < parseInt(args[i])) {
-      n = parseInt(args[i]);
+    pi = parseInt(args[i]);
+    if (isNaN(pi) === false) {
+      if (n < pi) {
+        n = pi;
+      }
     }
   }
   return (n);
