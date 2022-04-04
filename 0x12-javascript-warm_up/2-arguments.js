64040dcb344c99d16f7d909c3279116c732e2c60
Anna78990 <takasuanna1990@gmail.com>
2022-04-04 12:25:57 +0000

    for task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-let argv = process.argv;
-if (argv[2])
-	console.log("Argument found")
-else
-        console.log("No argument")
+const argv = process.argv;
+if (argv[2]) {
+  if (argv[3]) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
+} else {
+  console.log('No argument');
+}
