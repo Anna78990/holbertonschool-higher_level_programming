cc9de5fe228db4a801b2552cf250238480c32e43
Anna78990 <takasuanna1990@gmail.com>
2022-04-05 12:04:03 +0000

    push in bulk

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'x') {
+    if (this.width !== undefined) {
+      const arr = [];
+      for (let i = 0; i < this.width; i++) {
+        arr.push(c);
+      }
+      const arr2 = arr.join('');
+      for (let j = 0; j < this.width; j++) {
+        console.log(arr2);
+      }
+    }
+  }
+};
