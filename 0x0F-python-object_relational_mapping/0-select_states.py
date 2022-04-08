54274e0f942739fc27084c0b58a4a9d4bb6f24a9
Anna78990 <takasuanna1990@gmail.com>
2022-04-08 22:02:20 +0000

    0,1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 arg = sys.argv
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-    passwd=arg[2], db=arg[3], charset="utf8")
+                       passwd=arg[2], db=arg[3], charset="utf8")
 
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,10 +5,11 @@ import MySQLdb
 arg = sys.argv
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-    passwd=arg[2], db=arg[3], charset="utf8")
+                       passwd=arg[2], db=arg[3], charset="utf8")
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE SUBSTR(name, 1, 1) = 'N' ORDER BY id ASC")
+cur.execute("SELECT * FROM states \
+            WHERE SUBSTR(name, 1, 1) = 'N' ORDER BY id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
