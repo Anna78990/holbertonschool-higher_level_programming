5795b227cbabf497bd87e32c46448eda3dbd50ec
Anna78990 <takasuanna1990@gmail.com>
2022-04-08 21:59:38 +0000

    34

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,10 +5,11 @@ import MySQLdb
 arg = sys.argv
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-    passwd=arg[2], db=arg[3], charset="utf8")
+                       passwd=arg[2], db=arg[3], charset="utf8")
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name = %(name)s ORDER BY id ASC", {'name': arg[4]})
+cur.execute("SELECT * FROM states \
+            WHERE name = %(name)s ORDER BY id ASC", {'name': arg[4]})
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,10 +5,12 @@ import MySQLdb
 arg = sys.argv
 
 conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-    passwd=arg[2], db=arg[3], charset="utf8")
+                       passwd=arg[2], db=arg[3], charset="utf8")
 
 cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY id;")
+cur.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
+            ORDER BY id;")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
