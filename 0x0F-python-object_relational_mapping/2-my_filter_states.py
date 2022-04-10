5f844c68bcf8be5c7129b866dd78869f04cbeb4b
Anna78990 <takasuanna1990@gmail.com>
2022-04-10 20:34:30 +0000

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""
+displays all values where name matches the argument.
+"""
 import sys
 import MySQLdb
 
-arg = sys.argv
-conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-                       passwd=arg[2], db=arg[3], charset="utf8")
-
-cur = conn.cursor()
-cur.execute("SELECT * FROM states \
-            WHERE name = '%s' ORDER BY id ASC" % arg[4])
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    arg = sys.argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
+                           passwd=arg[2], db=arg[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name = '%s' ORDER BY id ASC" % arg[4])
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""
+SQL Injection for 2-my_filter_states.py
+"""
+
 import sys
 import MySQLdb
 
-arg = sys.argv
-
-conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-                       passwd=arg[2], db=arg[3], charset="utf8")
-
-cur = conn.cursor()
-cur.execute("SELECT * FROM states \
-            WHERE name = %(name)s ORDER BY id ASC", {'name': arg[4]})
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    arg = sys.argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
+                           passwd=arg[2], db=arg[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name = %(name)s ORDER BY id ASC", {'name': arg[4]})
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
