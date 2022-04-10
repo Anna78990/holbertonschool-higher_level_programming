825bd5a776434c34c300c091bac0636f217f2547
Anna78990 <takasuanna1990@gmail.com>
2022-04-10 22:14:02 +0000

    join

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+
 """
 import sys
 from model_state import Base, State
@@ -12,8 +13,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(a[1], a[2], a[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-session = Session(engine)
-for join in session.query(State, City).\
-  join(City, State.id == City.state_id).order_by(City.id).all():
-    print("{} :({}) {}".format(join.State.name, join.City.id, join.City.name))
-session.close()
+    session = Session(engine)
+    for join in session.query(State, City)\
+            .join(City, State.id == City.state_id)\
+            .order_by(City.id).all():
+        print("{} :({}) {}".format(join.State.name,
+                                   join.City.id, join.City.name))
+    session.close()
