b02b5b8b803e4c082632ad3905edcb00e877e89c
Anna78990 <takasuanna1990@gmail.com>
2022-04-10 21:54:46 +0000

    dlt

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,10 +12,8 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        if (state.name.find('a') != -1) and (state.name.find('A') != -1):
-            session.query(State).filter(State.id == state.id).\
-                                        delete(synchronize_session=False)
+    session = Session(bind=engine)
+    dl = session.query(State).filter(State.name.like("%a%"))
+    dl.delete()
     session.commit()
     session.close()
