cf01dac55590aa03cfea127253f75261098ebea5
Anna78990 <takasuanna1990@gmail.com>
2022-04-10 20:19:13 +0000

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,15 @@ lists all states with a name starting with N
 import sys
 import MySQLdb
 
-arg = sys.argv
-conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
-                       passwd=arg[2], db=arg[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states \
-            WHERE SUBSTR(name, 1, 1) = 'N' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    arg = sys.argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
+                           passwd=arg[2], db=arg[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE SUBSTR(name, 1, 1) = 'N' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
