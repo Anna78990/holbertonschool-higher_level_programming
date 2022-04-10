c063ba3034f8de6137a502538ea8aac4f1669ea3
Anna78990 <takasuanna1990@gmail.com>
2022-04-10 22:15:38 +0000

    doc

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+Class City
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
 from model_state import Base, State
 
 class City(Base):
+    """
+    class City, an inherit class of Base
+    """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128))
