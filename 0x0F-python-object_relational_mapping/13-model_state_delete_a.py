2c67ad948a7a10f7f970be9bbf2c4d72ab5ef796
Anna78990 <takasuanna1990@gmail.com>
2022-04-10 21:28:55 +0000

    213

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+deletes all State objects with a name containing the letter a
 """
 import sys
 from model_state import Base, State
@@ -11,10 +12,9 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-session = Session(engine)
-if (State):
+    session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
         if (state.name.find('a') != -1) or (state.name.find('A') != -1):
             session.query(State).filter(State.id == state.id).delete()
-session.commit()
-session.close()
+    session.commit()
+    session.close()
