d207350c7547ffcdb1a14377d1d101c4bc3cd8fe
Anna78990 <takasuanna1990@gmail.com>
2022-04-11 21:48:41 +0000

    bind

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(a[1], a[2], a[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
+    session = Session(bind=engine)
     for join in session.query(State, City)\
             .join(City, State.id == City.state_id)\
             .order_by(City.id).all():
