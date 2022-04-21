1efa6cf1c8b3073c8b5779fa22292ad97bb463aa
Anna78990 <takasuanna1990@gmail.com>
2022-04-21 21:03:18 +0000

    foofle->

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept.
-curl -X OPTIONS -s google.fr -i | sed -n 2P | cut -d " " -f2- 
+curl -X OPTIONS -s "$1" -i | sed -n 2P | cut -d " " -f2- 
