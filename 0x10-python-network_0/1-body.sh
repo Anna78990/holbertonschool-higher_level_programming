222a501161a32698e4f0b3b113368bb585207ce7
Anna78990 <takasuanna1990@gmail.com>
2022-04-21 12:47:48 +0000

    300 -> 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays the body of the response if the status is 200
-if [ "$(curl -sI "$1" | head -1 | awk '{print $2}')" -eq 301 ]; then  curl -L "$1";fi
+if [ "$(curl -sI "$1" | head -1 | awk '{print $2}')" -eq 200 ]; then  curl -L "$1";fi
