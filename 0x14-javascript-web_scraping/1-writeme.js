af2fe3bd45956a6c25ac316a4033a96e43a2930e
Anna78990 <takasuanna1990@gmail.com>
2022-05-17 15:20:47 +0000

    supprime \ufeff

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = process.argv[2];
 const contents = process.argv[3];
-fs.writeFile(path, '\ufeff' + contents, err => {
+fs.writeFile(path, contents, err => {
   if (err) {
     console.error(err);
   }
