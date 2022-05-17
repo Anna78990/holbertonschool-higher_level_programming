2c8865c8cd9d667e0940ed9643ee0813f31bbc02
Anna78990 <takasuanna1990@gmail.com>
2022-05-17 15:22:36 +0000

    remove space

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const https = require('https');
 const path = process.argv[2];
 
 https.get(path, function (res) {
-  console.log('code: ', res.statusCode); // <======= Here's the status code
+  console.log('code:', res.statusCode); // <======= Here's the status code
 });
