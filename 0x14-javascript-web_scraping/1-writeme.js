896c58779fd4264cb152dd411c1fe5ba26c28d9a
Anna78990 <takasuanna1990@gmail.com>
2022-05-17 15:18:39 +0000

    supprime \n

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = process.argv[2];
 const contents = process.argv[3];
-fs.writeFile(path, '\ufeff' + contents + '\n', err => {
+fs.writeFile(path, '\ufeff' + contents, err => {
   if (err) {
     console.error(err);
   }
